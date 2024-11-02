37ea94419244a81e6c333d68ddeda0a8b441e858
MikaelReichiSopany <111833130+MikaelReichiSopany@users.noreply.github.com>
2024-11-02 20:19:16 +0700

    Update model_summary.py

--- model_summary.py
+++ model_summary.py
@@ -3,8 +3,8 @@ import pymongo
 import pandas as pd
 
 
-df_gru = pd.read_excel('model_summary/GRU.xlsx')
-df_rf = pd.read_excel('model_summary/RF.xlsx')
+df_gru = pd.read_excel('model_summary/gru.xlsx')
+df_rf = pd.read_excel('model_summary/rf.xlsx')
 
 st.subheader('Model summary for GRU')
 st.dataframe(df_gru)
